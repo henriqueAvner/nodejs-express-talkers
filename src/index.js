4c5d90445238907ffbba160886354ff5c4077017
henriqueAvner <avnerhdpb@gmail.com>
2023-12-05 17:44:10 -0300

    Feat: Requisito 2

--- src/index.js
+++ src/index.js
@@ -37,3 +37,16 @@ app.get('/talker/', async (_req, res) => {
     res.status(200).send([]);
   }
 });
+
+app.get('/talker/:id', async (req, res) => {
+
+  const { id } = req.params;
+  const allTalkers = await readJson();
+  const currTalker = allTalkers.find((talker) => talker.id === +id);
+
+  if (currTalker !== undefined) {
+    res.status(200).json(currTalker);
+  } else {
+    res.status(404).json({ message: "Pessoa palestrante não encontrada" });
+  }
+})
